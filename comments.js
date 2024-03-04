2054fd41dc54633951e7cc41223b78eb72b2d2d1
sebashsiao <111330065+sebashsiao@users.noreply.github.com>
2024-03-04 19:40:44 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,8 +1,20 @@
 // Create web server
-const express = require('express');
-const app = express();
-const port = 3000;
+// Create a new file comments.js and write the following code into it. This code will create a new web server and listen on port 8080. Also, you will create a new route /comments that will return JSON data.
 
-// Create comments array
-const comments = [
-    {username
+// comments.js
+
+var http = require('http');
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {"Content-Type": "application/json"});
+  res.end(JSON.stringify({message: 'Hello World'}));
+});
+server.listen(8080);
+
+// Run the server
+// Open the command prompt and run the comments.js file using the following command.
+
+// node comments.js
+// Output
+// This will produce the following output.
+
+// Server running at http://
